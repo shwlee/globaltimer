03d937878a93c4f164249859e3cb0fbbc6b28f03
shlee <shlee@ksnc.io>
2018-10-16 14:25:51 +0900

    - add custom exception

---
+++ GlobalSchedulerTest/NoMatchedMemberException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Security;
+
+namespace GlobalSchedulerTest
+{
+	[Serializable]
+	public class NoMatchedMemberException : Exception
+	{
+		public NoMatchedMemberException()
+		{
+		}
+
+		public NoMatchedMemberException(string message)
+			: base(message)
+		{
+		}
+
+		public NoMatchedMemberException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
+
+		public NoMatchedMemberException(SerializationInfo info, StreamingContext context)
+			: base(info, context)
+		{
+		}
+
+		[SecurityCritical]
+		public override void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			base.GetObjectData(info, context);
+		}
+	}
+}
